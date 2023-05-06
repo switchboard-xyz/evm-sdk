025a5c5631260d2250f767aaed235f24d242362d
Albert Hermida <alberthermida@gmail.com>
2023-05-06 01:08:18 -0400

    delete unnecessary bindings

--- javascript/evm.js/src/index.ts
+++ javascript/evm.js/src/index.ts
@@ -4,9 +4,4 @@ export {
   Switchboard__factory,
   SwitchboardVS,
   SwitchboardVS__factory,
-  CoreSwitchboard,
-  CoreSwitchboardV3,
-  CoreSwitchboardV3__factory,
-  CoreSwitchboardVS,
-  CoreSwitchboardVS__factory,
 } from "./typechain-types";
