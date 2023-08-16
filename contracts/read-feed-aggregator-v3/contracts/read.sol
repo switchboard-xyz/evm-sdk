4b4575f14f114b864f5dd3c397518caef564780a
Albert Hermida <alberthermida@gmail.com>
2023-08-16 14:11:24 -0400

    update interfaces, add links, fix some basic reading examples

--- contracts/read-feed-aggregator-v3/contracts/read.sol
+++ contracts/read-feed-aggregator-v3/contracts/read.sol
@@ -4,47 +4,45 @@ pragma solidity ^0.8.0;
 import "./ILegacyOracle.sol"; // AggregatorV3Interface
 
 contract ReadAFeed {
-  
-  // version of this contract
-  int256 public latestValue;
-  uint256 public latestTimestamp;
-  address switchboardAddress;
-  address aggregatorAddress;
+    // version of this contract
+    int256 public latestValue;
+    uint256 public latestTimestamp;
+    address switchboardAddress;
+    address aggregatorAddress;
 
-  // constructor
-  constructor(address _switchboard, address _aggregatorAdapterAddress) {
-    switchboardAddress = _switchboard;
-    aggregatorAddress = _aggregatorAdapterAddress;
-  }
+    // constructor
+    constructor(address _switchboard, address _aggregatorAdapterAddress) {
+        switchboardAddress = _switchboard;
+        aggregatorAddress = _aggregatorAdapterAddress;
+    }
 
-  function latest() external view returns (int256, uint256) {
-    return (latestValue, latestTimestamp);
-  }
+    function latest() external view returns (int256, uint256) {
+        return (latestValue, latestTimestamp);
+    }
 
-  function getLatestResult()
-    external
-    returns (
-      int256 value,
-      uint256 timestamp
-    ) {
+    function getLatestResult()
+        external
+        returns (int256 value, uint256 timestamp)
+    {
+        AggregatorV3Interface aggregator = AggregatorV3Interface(
+            aggregatorAddress
+        );
 
-      AggregatorV3Interface aggregator = AggregatorV3Interface(aggregatorAddress);
+        // fetch the latest round
+        (
+            ,
+            /*uint80 roundId*/ int256 answer,
+            uint256 startedAt /*uint256 updatedAt*/ /*uint80 answeredInRound*/,
+            ,
 
-      // fetch the latest round
-      (
-      /*uint80 roundId*/,
-      int256 answer,
-      uint256 startedAt,
-      /*uint256 updatedAt*/,
-      /*uint80 answeredInRound*/
-      ) = aggregator.latestRoundData();
+        ) = aggregator.latestRoundData();
 
-      // set these loocally
-      latestTimestamp = startedAt;
-      latestValue = answer;
+        // set these loocally
+        latestTimestamp = startedAt;
+        latestValue = answer;
 
-      // return these
-      value = answer;
-      timestamp = startedAt;
+        // return these
+        value = answer;
+        timestamp = startedAt;
     }
 }
--- contracts/read-feed/contracts/read.sol
+++ contracts/read-feed/contracts/read.sol
@@ -4,33 +4,34 @@ pragma solidity ^0.8.0;
 import "./ISwitchboard.sol";
 
 contract ReadAFeed {
-  
-  // version of this contract
-  int256 public latestValue;
-  uint256 public latestTimestamp;
-  address switchboardAddress;
-  address aggregatorAddress;
+    // version of this contract
+    int256 public latestValue;
+    uint256 public latestTimestamp;
+    address switchboardPushAddress;
+    address feedId;
 
-  // constructor
-  constructor(address _switchboard, address _aggregatorAddress) {
-    switchboardAddress = _switchboard;
-    aggregatorAddress = _aggregatorAddress;
-  }
+    // constructor
+    constructor(address _switchboard, address _feedId) {
+        switchboardPushAddress = _switchboard;
+        feedId = _feedId;
+    }
 
-  function latest() external view returns (int256, uint256) {
-    return (latestValue, latestTimestamp);
-  }
+    function latest() external view returns (int256, uint256) {
+        return (latestValue, latestTimestamp);
+    }
 
-  function getLatestResult()
-    external
-    returns (
-      int256 value,
-      uint256 timestamp
-    ) {
-      
-      ISwitchboard switchboard = ISwitchboard(switchboardAddress);
-      (value, timestamp) = switchboard.latestResult(aggregatorAddress);
-      latestValue = value;
-      latestTimestamp = timestamp;
+    function getLatestResult()
+        external
+        returns (int256 value, uint256 timestamp)
+    {
+        ISwitchboard switchboard = ISwitchboard(switchboardPushAddress);
+        (
+            int256 value,
+            uint256 timestamp,
+            uint256 _updatedAt,
+            uint80 _intervalId
+        ) = switchboard.getLatestResult(feedId);
+        latestValue = value;
+        latestTimestamp = timestamp;
     }
 }
